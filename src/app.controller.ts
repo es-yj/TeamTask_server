9ea4d05a44399c1d652b74179830849c592793d4
enxxi <zhes4593@gmail.com>
2024-03-15 15:28:48 +0900

    feat: 구글 oAuth 로그인 구현

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { AppService } from './app.service';
+import { GoogleOAuthGuard } from './auth/google-oauth.guard';
+
+@Controller('auth')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get()
+  @UseGuards(GoogleOAuthGuard)
+  async googleAuth(@Request() req) {}
+
+  @Get('google-redirect')
+  @UseGuards(GoogleOAuthGuard)
+  googleAuthRedirect(@Request() req) {
+    return this.appService.googleLogin(req);
+  }
+}
