87ed588d86d54d76f9c99d72504cede5e6deb33f
enxxi <zhes4593@gmail.com>
2024-03-15 17:01:50 +0900

    feat: 구글 로그인 파일 분리(auth)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 import { GoogleStrategy } from './auth/google.strategy';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService, GoogleStrategy],
+  controllers: [AppController, AuthController],
+  providers: [AppService, GoogleStrategy, AuthService],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { GoogleOAuthGuard } from './google-oauth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  @UseGuards(GoogleOAuthGuard)
+  async googleAuth(@Request() req) {}
+
+  @Get('google-redirect')
+  @UseGuards(GoogleOAuthGuard)
+  googleAuthRedirect(@Request() req) {
+    return this.authService.googleLogin(req);
+  }
+}
