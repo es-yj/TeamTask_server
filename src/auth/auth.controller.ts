704ae063dabcbe2e7207f652dbd12f04f156fe28
enxxi <zhes4593@gmail.com>
2024-03-19 17:41:42 +0900

    docs: 스웨거 설정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { GoogleOAuthGuard } from './google-oauth.guard';
+import { GoogleOAuthGuard } from './strategy/google-oauth.guard';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -10,6 +12,7 @@ export class AuthController {
   @UseGuards(GoogleOAuthGuard)
   async googleAuth(@Request() req) {}
 
+  @ApiOperation({ summary: '구글 로그인 요청' })
   @Get('google-redirect')
   @UseGuards(GoogleOAuthGuard)
   async googleAuthRedirect(@Request() req) {
---
+++ src/config/swagger.ts
@@ -0,0 +1,32 @@
+import { INestApplication } from '@nestjs/common';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerCustomOptions,
+} from '@nestjs/swagger';
+
+const SwaggerCustomOptions: SwaggerCustomOptions = {
+  swaggerOptions: {
+    persistAuthorization: true,
+  },
+};
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('money_bridge')
+    .setDescription('Swagger API 서버')
+    .setVersion('1.0.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'JWT',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document, SwaggerCustomOptions);
+}
