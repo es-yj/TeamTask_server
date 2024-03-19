948c32645e64787898a004c6df0dd6b1355e9507
enxxi <zhes4593@gmail.com>
2024-03-19 18:08:47 +0900

    config: 개발 환경 포트 변경

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setupSwagger(app);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3000;
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
